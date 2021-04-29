2351e621155e962a81ddf42c2f10e461cbd90cb1
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-28 23:01:40 -0400

    test to check if the given corrdinates has ship in it

--- test/ship_test.js
+++ test/ship_test.js
@@ -3,7 +3,19 @@ const expect = require("chai").expect;
 // new suite
 describe("checkForShip", () => {
   const checkForShip = require("../game_logic/ship_methods").checkForShip;
+  //   spec
   it("should correcly report no ship at a given players coordinate", () => {
+    let player = {
+      ships: [
+        {
+          locations: [[0, 0]],
+        },
+      ],
+    };
+    expect(checkForShip(player, [9, 9])).to.be.false;
+    expect(checkForShip(player, [0, 0])).to.be.ok;
+  });
+  it("should correctly report a ship located at the given coordinates", () => {
     let player = {
       ships: [
         {
